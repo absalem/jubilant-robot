2d49ddf0d897cc07a5884fc97bbd73d60dd7e041
Christian Weingärtner <cweingaertner@menadwork.com>
2016-01-22 11:13:56 +0100

    test

---
+++ default_boilerplate/customerDataTwig.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this TwigWrapper */
+
+use cms\twig\TwigWrapper;
+
+// #############################################################################
+// # TwigWrapper | extra content
+// #
+// # This file will be included automatically by TwigWrapper
+// # and the content is available for all templates of this theme-directory.
+// #
+// #############################################################################
+
+
+// $this <- because we include it in the TwigWrapper-Class !!!!
+//$this->assign('lall', $lall);
---
+++ default_boilerplate/js-min/app.js
@@ -0,0 +1,6 @@
+/*
+ * maw-cms-theme by menadwork GmbH (http://www.menadwork.com)
+ */
+
+$(document).ready(function(){});
+//# sourceMappingURL=app.js.map
